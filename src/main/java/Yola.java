0e9c26070a07906884f143d590531a39414f2f1c
5iu5 <e1384442@u.nus.edu>
2026-01-25 01:53:00 +0800

    Rename chatbot and add greeting messages

---
+++ src/main/java/Yola.java
@@ -0,0 +1,18 @@
+public class Yola {
+    public static void main(String[] args) {
+
+        String logo =    " __   __      _       \n"
+                        + " \\ \\ / /___  | | __ _ \n"
+                        + "  \\ V // _ \\ | |/ _` |\n"
+                        + "   | || (_) || | (_| |\n"
+                        + "   |_| \\___/ |_|\\__,_|\n";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Yola");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
