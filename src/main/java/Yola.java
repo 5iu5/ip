e6b6c8d26c28143029ec06e671ecce17a891296e
5iu5 <e1384442@u.nus.edu>
2026-01-28 01:13:09 +0800

    Add Mark as Done functionality
    Add the ability to mark tasks as done, as well as unmarking tasks.

--- src/main/java/Yola.java
+++ src/main/java/Yola.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Yola {
+
+
     public static void main(String[] args) {
 
         String logo =    " __   __      _       \n"
@@ -17,24 +19,59 @@ public class Yola {
 
         String line;
         Scanner in = new Scanner(System.in);
-        String[] textList = new String[100];
-        int listSize = 0;
+        Task[] taskList = new Task[100];
+        int size = 0;
 
         while(true){
             line = in.nextLine();
+            String[] words = line.strip().split("\\s+");
             if (line.equals("bye")){
                 break;
             }
+
             if (line.equals("list")){
                 System.out.println("    ____________________________________________________________");
-                for (int i = 0; i < listSize; i += 1){
-                    System.out.println("     " + (i+1)+ ". " + textList[i]);
+                System.out.println("    Here are the tasks in your list:");
+                for (int i = 0; i < size; i += 1){
+                    System.out.println("    " + (i+1)+ ".["+ taskList[i].getStatusIcon() + "] " + taskList[i].description);
                 }
                 System.out.println("    ____________________________________________________________");
             }
+
+            else if((words[0].equals("mark") || words[0].equals("unmark")) && words.length == 2){
+                //Check if it is a mark/unmark command
+
+
+                try {
+                     int taskNum = Integer.parseInt(words[1]);
+                     if (taskNum < 1 || taskNum > size) {
+                         System.out.println("invalid task number");
+                         continue;
+                     }
+                     Task t = taskList[taskNum-1];
+                    System.out.println("    ____________________________________________________________");
+
+                    if (words[0].equals("mark")){
+                         //mark task
+                         t.markDone();
+                         System.out.println("    Nice! I've marked this task as done:");
+                    }
+                     else {
+                         //unmark task
+                         t.unmark();
+                         System.out.println("        OK, I've marked this task as not done yet:");
+                     }
+                     System.out.println("      " + "[" + t.getStatusIcon() + "] " + t.description);
+                     System.out.println("    ____________________________________________________________");
+
+                }catch (NumberFormatException e) {
+                    System.out.println("Please enter a valid task number.");
+                }
+            }
             else{
-                textList[listSize] = line;
-                listSize += 1;
+                Task t = new Task(line);
+                taskList[size] = t;
+                size += 1;
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     added: " + line);
                 System.out.println("    ____________________________________________________________");
