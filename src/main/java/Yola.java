65b6efb49afc838ab5f69535e9c36421751f50e8
5iu5 <e1384442@u.nus.edu>
2026-01-27 13:19:03 +0800

    Add echo functionality to chatbot
    chatbot echoes user's messages until user types 'bye'

--- src/main/java/Yola.java
+++ src/main/java/Yola.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Yola {
     public static void main(String[] args) {
 
@@ -8,11 +10,30 @@ public class Yola {
                         + "   |_| \\___/ |_|\\__,_|\n";
 
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Yola");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Yola");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while(true){
+            line = in.nextLine();
+            if (line.equals("bye")){
+                break;
+            }
+            System.out.println("    ____________________________________________________________");
+            System.out.print("    ");
+
+            System.out.println(line);
+            System.out.println("    ____________________________________________________________");
+        }
+
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+
     }
 }
