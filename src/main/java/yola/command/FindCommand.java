03ba9687e9e1d85154461706004e00c454272fc9
5iu5 <e1384442@u.nus.edu>
2026-03-08 21:17:18 +0800

    Add FindCommand class for finding task based on keyword

---
+++ src/main/java/yola/command/FindCommand.java
@@ -0,0 +1,33 @@
+package yola.command;
+
+import yola.exception.YolaException;
+import yola.model.TaskList;
+import yola.storage.StorageFile;
+import yola.task.Task;
+import yola.ui.Ui;
+
+import java.util.ArrayList;
+
+public class FindCommand extends Command{
+    private final String keyword;
+
+    public FindCommand(String keyword){
+        this.keyword = keyword;
+    }
+
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, StorageFile storage){
+        try {
+            if (keyword.isEmpty()) {
+                throw new YolaException("Keyword field is empty! Please enter a keyword to lookup.");
+            }
+
+            ArrayList<Task> matchingTasks = tasks.findTasks(keyword);
+            ui.printMatchingTasks(matchingTasks);
+
+        } catch (YolaException e) {
+            ui.printMessage(e.getMessage());
+        }
+    }
+}
