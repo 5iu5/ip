6cf7229436ee0f0d2c25d9029ecbbb95470b40d8
5iu5 <e1384442@u.nus.edu>
2026-03-08 19:13:33 +0800

    Add Parser class to handle parsing of user input
    Extract input parsing logic from Yola into a dedicated Parser class.
    Simplify Yola.main and improves code organization.

---
+++ src/main/java/yola/parser/Parser.java
@@ -0,0 +1,41 @@
+package yola.parser;
+import yola.command.Command;
+import yola.command.DeadlineCommand;
+import yola.command.DeleteCommand;
+import yola.command.EventCommand;
+import yola.command.ExitCommand;
+import yola.command.InvalidCommand;
+import yola.command.ListCommand;
+import yola.command.MarkCommand;
+import yola.command.TodoCommand;
+import yola.command.UnmarkCommand;
+
+public class Parser {
+    public static Command parse(String input){
+        // Split user input into command word and command body (the remaining line)
+        String[] commands = input.strip().split("\\s+", 2);
+        String commandWord = commands[0];
+        String commandBody = (commands.length > 1) ? commands[1] : "";
+
+        switch (commandWord) {
+        case "bye":
+            return new ExitCommand();
+        case "list":
+            return new ListCommand();
+        case "mark":
+            return new MarkCommand(commandBody);
+        case "unmark":
+            return new UnmarkCommand(commandBody);
+        case "delete":
+            return new DeleteCommand(commandBody);
+        case "todo":
+            return new TodoCommand(commandBody);
+        case "deadline":
+            return new DeadlineCommand(commandBody);
+        case "event":
+            return new EventCommand(commandBody);
+        default:
+            return new InvalidCommand();
+        }
+    }
+}
